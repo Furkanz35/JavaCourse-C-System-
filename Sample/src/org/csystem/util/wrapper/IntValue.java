699bb322b923975b05a8e1f30e71b93b071480f4
Furkan Zengin <furkan_zenginn_@hotmail.com>
2022-09-21 17:53:18 +0300

    IntValue class'ına basit methodlar eklendi

--- Sample/src/org/csystem/util/wrapper/IntValue.java
+++ Sample/src/org/csystem/util/wrapper/IntValue.java
@@ -5,6 +5,11 @@ public class IntValue {
     private static final int CACHE_MIN = -128;
     private static final int CACHE_MAX = 127;
 
+    public static final IntValue ZERO = of(0);
+    public static final IntValue ONE = of(1);
+    public static final IntValue TWO = of(2);
+    public static final IntValue TEN = of(10);
+
     private static final IntValue [] CACHE = new IntValue[CACHE_MAX - CACHE_MIN + 1];
 
     private final int m_value;
@@ -37,5 +42,40 @@ public class IntValue {
     {
         return "" + m_value;
     }
+
+    public IntValue add(int value)
+    {
+        return of(m_value + value);
+    }
+
+    public IntValue add(IntValue val)
+    {
+        return add(val.m_value);
+    }
+    public IntValue substract(int value)
+    {
+        return add(- value);
+    }
+
+    public IntValue substract(IntValue val)
+    {
+        return add(-val.m_value);
+    }
+
+    public IntValue multiply(int val)
+    {
+        return of(m_value * val);
+    }
+
+    public IntValue multiply(IntValue val)
+    {
+        return multiply(val.m_value);
+    }
+
+    public int compareTo(IntValue other)
+    {
+        return m_value - other.m_value;
+    }
+    
 
 }
