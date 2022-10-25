5c7bf064d62bbe1328d74b858123d31589105689
Furkan Zengin <furkan_zenginn_@hotmail.com>
2022-10-25 16:53:13 +0300

    Upcasting kullanılarak HumanResources classı içerisine nesne bağımsız olarak payInsurence methodu yazıldı

--- Sample/src/org/csystem/app/App.java
+++ Sample/src/org/csystem/app/App.java
@@ -1,15 +1,12 @@
 package org.csystem.app;
-import org.csystem.util.array.ArrayUtil;
-import org.csystem.util.datetime.Time;
-import org.csystem.util.math.MutableFraction;
 
-import java.sql.SQLOutput;
+
+import org.csystem.app.company.CompanyApp;
+
 import java.util.Arrays;
 
 class App {
     public static void main(String[] args) {
-
-        MutableFraction fraction = new MutableFraction(18 , -27);
-        System.out.println(fraction.toString());
-    }
+        CompanyApp.run();
+}
 }
